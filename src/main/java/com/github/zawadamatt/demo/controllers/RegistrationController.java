5d03cc1fbafb3a90351350e5e1e58b54be119711
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-11-05 20:57:58 +0100

    Add registration form.

---
+++ src/main/java/com/github/zawadamatt/demo/controllers/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.github.zawadamatt.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @GetMapping
+    public String prepareRegistrationPage() {
+        return "/WEB-INF/views/registration-form.jsp";
+    }
+
+    @PostMapping
+    public String processRegistrationPage(String username, String password, String firstName, String lastName) {
+        return "/WEB-INF/index.html";
+    }
+
+}
